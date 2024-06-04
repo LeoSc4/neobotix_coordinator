ee9497912fe52c1ad361accd0e1e4b09f49d7fa1
neobotix <neobotix@example.com>
2024-06-04 16:48:42 +0200

    adapt to neobotix ss24 project requirements

--- neobotix_coordinator/src/nodes/GetMarkerPosition.cpp
+++ neobotix_coordinator/src/nodes/GetMarkerPosition.cpp
@@ -27,15 +27,16 @@ GetMarkerPosition::GetMarkerPosition(const std::string &name, const BT::NodeConf
  */
 BT::PortsList GetMarkerPosition::providedPorts()
 {
-    return {BT::InputPort<std::string>("marker_frame"),
-            BT::InputPort<int>("max_seconds"),
-            BT::InputPort<std::string>("base_frame"), // default: "base_link" -> 
+    return {BT::InputPort<int>("aruco_id"), // default: 1
+            BT::InputPort<int>("max_seconds"),  // default: 5
+            BT::InputPort<std::string>("base_frame"), // default: "base_link"
             BT::OutputPort<float>("x"),
             BT::OutputPort<float>("y"),
             BT::OutputPort<float>("z"),
-            BT::OutputPort<float>("rotation_x"),
-            BT::OutputPort<float>("rotation_y"),
-            BT::OutputPort<float>("rotation_z")};
+            BT::OutputPort<float>("q_x"),
+            BT::OutputPort<float>("q_y"),
+            BT::OutputPort<float>("q_z"),
+            BT::OutputPort<float>("q_w")};
 }
 
 /**
@@ -65,9 +66,13 @@ BT::NodeStatus GetMarkerPosition::on_running()
 
             try
             {
-                // tf_buffer_->canTransform("base_link", marker_frame_);
+                std::string marker_frame = "aruco_" + std::to_string(ports.get_value<int>("aruco_id"));
+                RCLCPP_INFO(
+                    get_node_handle()->get_logger(), "Calculate transform from base_link to  %s", marker_frame);
+
+                // tf_buffer_->canTransform("base_link", aruco_{aruco_id});
                 transformStamped = tf_buffer_->lookupTransform(
-                    ports.get_value<std::string>("base_frame"), ports.get_value<std::string>("marker_frame"), // get_node_handle()->get_clock()->now(), 500ms);
+                    ports.get_value<std::string>("base_frame"), marker_frame, // get_node_handle()->get_clock()->now(), 500ms);
                     tf2::TimePointZero);
             }
             catch (tf2::TransformException &ex)
@@ -75,95 +80,31 @@ BT::NodeStatus GetMarkerPosition::on_running()
                 RCLCPP_INFO(
                     get_node_handle()->get_logger(), "Could not transform: %s", ex.what());
 
-                // log("Could not transform!" + ex.what());
                 return BT::NodeStatus::RUNNING;
             }
 
-            double x = 0, y = 0, z = 0, roll = 0, mean_roll = 0, pitch = 0, mean_pitch = 0, yaw = 0, mean_yaw = 0;
+            double x = 0, y = 0, z = 0, q_x = 0, q_y = 0, q_z = 0, q_w = 0;
 
-            if (marker_mean_.size() != 2)
-            {
-                marker_mean_.push_back(transformStamped);
-
-                tf2::Quaternion q(transformStamped.transform.rotation.x,
-                                  transformStamped.transform.rotation.y,
-                                  transformStamped.transform.rotation.z,
-                                  transformStamped.transform.rotation.w);
-
-                tf2::Matrix3x3 m(q);
-                m.getRPY(roll, pitch, yaw);
-                roll = RAD2DEG(roll);
-                pitch = RAD2DEG(pitch);
-                yaw = RAD2DEG(yaw);
-
-                marker_mean_.back().transform.rotation.x = roll;
-                marker_mean_.back().transform.rotation.y = pitch;
-                marker_mean_.back().transform.rotation.z = yaw;
-                log(std::to_string(transformStamped.transform.translation.x));
-
-                log("Marker Pose " + std::to_string(marker_mean_.size()));
-                log("MarkerPosition: (x=" + Converter::ftos(transformStamped.transform.translation.x) + ", y=" + Converter::ftos(transformStamped.transform.translation.y) + ", z=" + Converter::ftos(transformStamped.transform.translation.z) + ")");
-                log("MarkerPosition: (roll=" + Converter::ftos(roll) + ", pitch=" + Converter::ftos(pitch) + ", yaw=" + Converter::ftos(yaw) + ")");
-
-                return BT::NodeStatus::RUNNING;
-            }
-
-            for (auto &&marker : marker_mean_)
-            {
-                x += marker.transform.translation.x;
-                y += marker.transform.translation.y;
-                z += marker.transform.translation.z;
-
-                mean_roll += marker.transform.rotation.x;
-                mean_pitch += marker.transform.rotation.y;
-                mean_yaw += marker.transform.rotation.z;
-                // log(std::to_string(mean_roll));
-            }
-            // log(std::to_string(marker_mean_.size()));
-
-            log(std::to_string(x));
-            log(std::to_string(marker_mean_.size()));
-
-            x = x / marker_mean_.size();
-            y = y / marker_mean_.size();
-            z = z / marker_mean_.size();
-            mean_roll = mean_roll / marker_mean_.size();
-            mean_pitch = (mean_pitch / marker_mean_.size()); //- 90.0;
-            mean_yaw = (mean_yaw / marker_mean_.size());     //+ 180.0;
+            x = transformStamped.transform.translation.x;
+            y = transformStamped.transform.translation.y;
+            z = transformStamped.transform.translation.z; 
+            
+            q_x = transformStamped.transform.rotation.x;
+            q_y = transformStamped.transform.rotation.y;
+            q_z = transformStamped.transform.rotation.z;
+            q_w = transformStamped.transform.rotation.w;
 
             ports.set_value<float>("x", x);
             ports.set_value<float>("y", y);
             ports.set_value<float>("z", z);
-            ports.set_value<float>("rotation_x", mean_roll);
-            ports.set_value<float>("rotation_y", mean_pitch);
-            ports.set_value<float>("rotation_z", mean_yaw);
+            ports.set_value<float>("q_x", q_x);
+            ports.set_value<float>("q_y", q_y);
+            ports.set_value<float>("q_z", q_z);
+            ports.set_value<float>("q_w", q_w);
 
-            log("Mean Marker Pose");
+            log("Marker Pose relative to base_link/ world Frame: ");
             log("MarkerPosition: (x=" + Converter::ftos(x) + ", y=" + Converter::ftos(y) + ", z=" + Converter::ftos(z) + ")");
-            log("MarkerPosition: (roll=" + Converter::ftos(mean_roll) + ", pitch=" + Converter::ftos(mean_pitch) + ", yaw=" + Converter::ftos(mean_yaw) + ")");
-
-            tf_publisher_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(get_node_handle());
-            rclcpp::Time now = get_node_handle()->get_clock()->now();
-            geometry_msgs::msg::TransformStamped t;
-
-            t.header.stamp = now;
-            t.header.frame_id = "base_link";
-            t.child_frame_id = "mean_marker";
-
-            t.transform.translation.x = x;
-            t.transform.translation.y = y + OFFSET_Y;
-            t.transform.translation.z = z;
-            tf2::Quaternion q;
-            q.setRPY(
-                DEG2RAD(mean_roll),
-                DEG2RAD(mean_pitch),
-                DEG2RAD(mean_yaw));
-            t.transform.rotation.x = q.x();
-            t.transform.rotation.y = q.y();
-            t.transform.rotation.z = q.z();
-            t.transform.rotation.w = q.w();
-
-            tf_publisher_->sendTransform(t);
+            log("MarkerPosition: (q_x=" + Converter::ftos(q_x) + ", q_y=" + Converter::ftos(q_y) + ", q_z=" + Converter::ftos(q_z) + ", q_w=" + Converter::ftos(q_w) + ")");
 
             return BT::NodeStatus::SUCCESS;
         }
